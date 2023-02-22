eeebc400eeb592425e9db5b4be5474de920aa3f5
siaoynli <siaoynli@126.com>
2023-02-22 11:10:31 +0800

    feat(custom): 添加router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import 'virtual:windi.css'
+import router from './router'
 import App from './App.vue'
+
 const app = createApp(App)
 app.use(ElementPlus)
-import 'virtual:windi.css'
+app.use(router)
+
 app.mount('#app')
