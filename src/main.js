e215db6f441feca76cafbef893bb95fe3b096a0c
siaoynli <siaoynli@126.com>
2023-02-22 11:06:10 +0800

    feat(custom): 添加vue-router

--- src/main.js
+++ src/main.js
@@ -3,11 +3,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import App from './App.vue'
-
 const app = createApp(App)
-
 app.use(ElementPlus)
-
 import 'virtual:windi.css'
-
 app.mount('#app')
