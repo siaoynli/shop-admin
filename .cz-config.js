2d9bc14f14b975b360a77f6997a3059eb5a02efb
siaoynli <siaoynli@126.com>
2023-03-09 10:32:03 +0800

    style(page): cz config

--- .cz-config.js
+++ .cz-config.js
@@ -3,12 +3,12 @@ module.exports = {
   types: [
     { value: 'feat', name: '✨ feat:  新功能' },
     { value: 'fix', name: '🐛 fix:  修复bug' },
-    { value: 'docs', name: '📖 docs:  文档变更' },
-    { value: 'style', name: '💄 style:  代码的样式美化' },
     {
       value: 'refactor',
       name: '🔁 refactor:  重构(既不是增加feature，也不是修复bug)'
     },
+    { value: 'docs', name: '📖 docs:  文档变更' },
+    { value: 'style', name: '💄 style:  代码的样式美化' },
     { value: 'perf', name: '⚡️ perf:  性能优化' },
     { value: 'test', name: '✅ test:  增加测试' },
     { value: 'chore', name: '🚀 chore:  构建过程或辅助工具的变动' },
