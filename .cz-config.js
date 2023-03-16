a28c33dabf20016447c9b33d31c3a60d930814ba
siaoynli <siaoynli@126.com>
2023-03-16 14:21:01 +0800

    style(doc): change cz-config

--- .cz-config.js
+++ .cz-config.js
@@ -2,19 +2,20 @@ module.exports = {
   // 可选类型
   types: [
     { value: 'feat', name: '✨ feat:  新功能' },
-    { value: 'fix', name: '🐛 fix:  修复bug' },
+    { value: 'fix', name: '👻 fix:  修复bug' },
     {
       value: 'refactor',
-      name: '🔁 refactor:  重构(既不是增加feature，也不是修复bug)'
+      name: '🍥 refactor:  重构(既不是增加feature，也不是修复bug)'
     },
-    { value: 'docs', name: '📖 docs:  文档变更' },
-    { value: 'style', name: '💄 style:  代码的样式美化' },
-    { value: 'perf', name: '⚡️ perf:  性能优化' },
-    { value: 'test', name: '✅ test:  增加测试' },
+    { value: 'docs', name: '📚 docs:  文档变更' },
+    { value: 'style', name: '📑 style:  代码的样式美化' },
+
+    { value: 'perf', name: '⏱ perf:  性能优化' },
+    { value: 'test', name: '🌀 test:  增加测试' },
     { value: 'chore', name: '🚀 chore:  构建过程或辅助工具的变动' },
-    { value: 'revert', name: '⏪️ revert:  回退' },
+    { value: 'revert', name: '🔙 revert:  回退' },
     { value: 'build', name: '📦️ build:  打包' },
-    { value: 'ci', name: '🧰 ci:  更改为我们的CI配置文件和脚本' }
+    { value: 'ci', name: '🤖 ci:  更改为我们的CI配置文件和脚本' }
   ],
 
   scopes: [
