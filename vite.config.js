97aa5aff951256a23a41b9db808ab68c780f2d51
siaoynli <siaoynli@126.com>
2023-03-17 14:03:14 +0800

    refactor(page): 删除无用代码

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,7 @@ export default defineConfig(configEnv => {
 
   return {
     server: {
+      open: true,
       proxy: createViteProxy(isOpenProxy, envConfig)
     },
     plugins: setupVitePlugins(viteEnv),
